dd313722a1dcc76472f38b06514d6cc49d27d6d4
Marcel Hecko <maco@blava.net>
2016-08-21 21:06:08 +0200

    saving of rawpoints has been borked

--- web_api/api/urls.py
+++ web_api/api/urls.py
@@ -5,11 +5,11 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^node/(.+)/?$', views.node_info),
+    url(r'^points/save/?$', views.save_point),
     url(r'^points/([0-9a-z]+)/([0-9]+)/?$', views.points_this_node_key),
     url(r'^points/([0-9a-z]+)/?$', views.points_this_node),
     url(r'^points/all/([0-9]+)/?$', views.points_all_nodes_key),
     url(r'^points/all/?$', views.points_all_nodes),
-    url(r'^points/save/?$', views.save_point),
     url(r'^rawpoints/([0-9a-z]+)/?$', views.rawpoints_this_node),
     url(r'^rawpoints/?$', views.rawpoints),
     url(r'^gis/?$', views.gis),
