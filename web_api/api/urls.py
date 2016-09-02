e0e5130c5236f1f499d35013807fbf800dd7ddd1
Marcel Hecko <maco@blava.net>
2016-09-02 15:34:12 +0200

    accept uppercase in api hash

--- web_api/api/urls.py
+++ web_api/api/urls.py
@@ -4,14 +4,14 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^node/([0-9a-z]+)/?$', views.node_info),
+    url(r'^node/([0-9a-zA-Z]+)/?$', views.node_info),
     url(r'^nodes/?$', views.nodes),
     url(r'^points/save/?$', views.save_point),
-    url(r'^points/([0-9a-z]+)/([0-9]+)/?$', views.points_this_node_key),
-    url(r'^points/([0-9a-z]+)/?$', views.points_this_node),
+    url(r'^points/([0-9a-zA-Z]+)/([0-9]+)/?$', views.points_this_node_key),
+    url(r'^points/([0-9a-zA-Z]+)/?$', views.points_this_node),
     url(r'^points/all/([0-9]+)/?$', views.points_all_nodes_key),
     url(r'^points/all/?$', views.points_all_nodes),
-    url(r'^rawpoints/([0-9a-z]+)/?$', views.rawpoints_this_node),
+    url(r'^rawpoints/([0-9a-zA-Z]+)/?$', views.rawpoints_this_node),
     url(r'^rawpoints/?$', views.rawpoints),
     url(r'^gis/?$', views.gis),
 ]
